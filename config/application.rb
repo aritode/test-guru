53f2cf110502377c07a814bd7b2bd6975dd50069
aritode <arian2k@gmail.com>
2018-10-09 12:25:07 +0300

    move VERSION constant to config/initializers

--- config/application.rb
+++ config/application.rb
@@ -7,12 +7,6 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module TestGuru
-  VERSION = '0.1.0'
-
-  def self.version
-    VERSION
-  end
-
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
---
+++ config/initializers/version.rb
@@ -0,0 +1,7 @@
+module TestGuru
+  VERSION = '0.1.0'.freeze
+
+  def self.version
+    VERSION
+  end
+end
