075b2fc8db91c58323057371a1dded382b07d61d
aritode <arian2k@gmail.com>
2018-10-18 22:30:39 +0300

    add Test.sort_by_category with reversed title order

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
+
+  def self.sort_by_category(category_title)
+    joins(:category)
+      .where(categories: { title: category_title })
+      .order('title desc')
+  end
 end
