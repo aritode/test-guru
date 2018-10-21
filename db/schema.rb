c4b332616d8181f7f8361097ede9d289bc6a05f4
aritode <arian2k@gmail.com>
2018-10-21 15:09:51 +0300

    add #author to Test and #tests_created to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_20_184452) do
+ActiveRecord::Schema.define(version: 2018_10_21_112153) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 2018_10_20_184452) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
