784c76f91aadd9b0b44cd9b4b721f98bc5d1d1d6
aritode <arian2k@gmail.com>
2018-10-26 20:12:45 +0300

    add scope methods for Test.level - easy/medium/hard

--- app/models/test.rb
+++ app/models/test.rb
@@ -7,6 +7,10 @@ class Test < ApplicationRecord
 
   belongs_to :author, class_name: 'User'
 
+  scope :easy,    -> { where(level: 0..1) }
+  scope :medium,  -> { where(level: 2..4) }
+  scope :hard,    -> { where(level: 5..Float::INFINITY) }
+
   def self.sort_by_category(category_title)
     joins(:category)
       .where(categories: { title: category_title })
