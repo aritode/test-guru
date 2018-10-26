4cf70ff0083d4c9401009375eda6782350ce93b7
aritode <arian2k@gmail.com>
2018-10-26 23:51:03 +0300

    add #validate_max_answers_count to Answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,5 +3,14 @@ class Answer < ApplicationRecord
 
   validates :body, presence: true
 
+  validate :validate_max_answers_count, on: :create
+
   scope :correct, -> { where(correct: true) }
+
+  private
+
+  def validate_max_answers_count
+    message = 'can only have maximum 4 answers!'
+    errors.add(:question, message) if question.answers.count >= 4
+  end
 end
