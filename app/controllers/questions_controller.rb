bc8bd76ef158ef52c4385545b4326f8a2f975667
aritode <arian2k@gmail.com>
2018-10-31 22:56:17 +0300

    add questions#create - Create a question with simple form (for test_id = 1)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :set_test, only: :index
+  before_action :set_test, only: [:index, :create]
 
   def index
     result = @test.questions.pluck(:body)
@@ -13,9 +13,26 @@ class QuestionsController < ApplicationController
     render plain: @question.body
   end
 
+  def new
+  end
+
+  def create
+    question = @test.questions.create(question_params)
+
+    result = ["test_id: #{@test.id}",
+              "Question id: #{question.id}",
+              "Question Body: #{question.body}"]
+
+    render plain: result.join("\n")
+  end
+
   private
 
   def set_test
     @test = Test.find(params[:test_id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
   end
 end
