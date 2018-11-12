aadc9e7bdd6d4e906f1f6ce60dcbb000e1b527f0
aritode <arian2k@gmail.com>
2018-11-12 21:01:10 +0300

    add layouts/footer with helpers: current_year and github_url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_year
+
+  def current_year
+    Date.today.year
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def github_url(author, repo = '')
+    result_body = repo.split(/[-_]/).map(&:capitalize).join
+
+    link_to result_body,
+            "https://github.com/#{author}/#{repo}",
+            target: '_blank'
+  end
 end
