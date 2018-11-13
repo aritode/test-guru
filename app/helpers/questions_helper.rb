62bdc1c099969fc2fdd64bb636d1a40b6412ac96
aritode <arian2k@gmail.com>
2018-11-13 22:06:31 +0300

    refactor question_header with question.new_record?

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,14 +1,11 @@
 module QuestionsHelper
   def question_header(question)
     test_title = question.test.title
-    action_name = controller.action_name
 
-    name = if action_name == 'edit'
-             "Edit #{test_title} Question"
-           elsif action_name == 'new'
+    name = if question.new_record?
              "Create New #{test_title} Question"
            else
-             "#{action_name.capitalize} #{test_title} Question"
+             "Edit #{test_title} Question"
            end
 
     content_tag('h1', name)
