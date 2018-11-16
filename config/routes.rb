65ee365f3c4e5f3e32440949ea90bb291a8f547c
aritode <arian2k@gmail.com>
2018-11-16 22:12:07 +0300

    create Answer resource and make it nested resource (scaffold styles)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+
+  root 'tests#index'
+
   resources :tests do
-    resources :questions, shallow: true, except: :index
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true
+    end
   end
+
+  # get '/tests/:category/:title', to: 'tests#search', level: 2
+
 end
