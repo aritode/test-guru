0df5631f733597742af490acf618c1f1e63cdb67
aritode <arian2k@gmail.com>
2018-11-18 17:58:47 +0300

    complete basic App logic with check answer and next/result actions

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -3,15 +3,19 @@ class TestPassagesController < ApplicationController
   before_action :set_test_passage, only: %i[show update result]
 
   def show
-
   end
 
   def result
-
   end
 
   def update
-    render :show
+    @test_passage.accept!(params[:answer_ids])
+
+    if @test_passage.completed?
+      redirect_to result_test_passage_path(@test_passage)
+    else
+      render :show
+    end
   end
 
   private
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -5,9 +5,37 @@ class TestPassage < ApplicationRecord
 
   before_validation :before_validation_set_first_question, on: :create
 
+  def completed?
+    current_question.nil?
+  end
+
+  def accept!(answer_ids)
+    if correct_answer?(answer_ids)
+      self.correct_questions += 1
+    end
+
+    self.current_question = next_question
+    save!
+  end
+
   private
 
   def before_validation_set_first_question
     self.current_question = test.questions.first if test.present?
+  end
+
+  def correct_answer?(answer_ids)
+    correct_answers_count = correct_answers.count
+
+    (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
+      correct_answers_count == answer_ids.count
+  end
+
+  def correct_answers
+    current_question.answers.correct
+  end
+
+  def next_question
+    test.questions.order(:id).where('id > ?', current_question.id).first
   end
 end
