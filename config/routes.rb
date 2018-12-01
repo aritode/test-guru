bae0752b90f84f0a15958b781d0a28d15a8dd60e
aritode <arian2k@gmail.com>
2018-12-01 17:58:23 +0300

    add sessions (CookieStorage) - SessionsController and SessionsViews

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   root 'tests#index'
 
   get :sign_up, to: 'users#new'
+  get :login,   to: 'sessions#new'
 
   resources :users, only: :create
+  resources :sessions, only: :create
 
   resources :tests do
     resources :questions, shallow: true, except: :index do
