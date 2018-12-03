c9a960a4f32267d9d6e0a093d108aa4e69a41bfa
aritode <arian2k@gmail.com>
2018-12-03 22:11:31 +0300

    add flash[:alert] message to login form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate_user!
     unless current_user
-      redirect_to login_path
+      redirect_to login_path, alert: 'Are you a Guru? Verify your Email and Password please'
     end
   end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to tests_path
     else
+      flash.now[:alert] = 'Are you a Guru? Verify your Email and Password please'
       render :new
     end
   end
