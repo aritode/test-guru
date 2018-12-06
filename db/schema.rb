73184043f8604b7a732f70344232a2e39d7d9abe
aritode <arian2k@gmail.com>
2018-12-06 21:00:58 +0300

    add DB-level validation - Email index (unique) to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_23_112007) do
+ActiveRecord::Schema.define(version: 2018_12_06_175627) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2018_11_23_112007) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
