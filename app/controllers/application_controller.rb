0ea5d50b519456bbd2206535ebcd2ede90223bb0
aritode <arian2k@gmail.com>
2018-12-06 23:36:35 +0300

    save intended_url in Cookie - so after login app redirects to intended_url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate_user!
     unless current_user
+      cookies[:intended_url] = request.url
       redirect_to login_path, alert: 'Are you a Guru? Verify your Email and Password please'
     end
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,10 +12,11 @@ class SessionsController < ApplicationController
     if user
       session[:user_id] = user.id
       flash[:notice] = "Welcome back, #{user.name}!"
+      redirect_to(cookies.delete(:intended_url) || tests_path)
     else
       flash.now[:alert] = 'Are you a Guru? Verify your Email and Password please'
+      redirect_to login_path
     end
-    redirect_to tests_path
   end
 
   def destroy
