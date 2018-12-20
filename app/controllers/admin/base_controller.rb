6692dcf175b56d0cfbdc78d347fa25eb7c850eab
aritode <arian2k@gmail.com>
2018-12-20 06:00:43 +0300

    create Admin::BaseController with #admin_required! callback

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,13 @@
+class Admin::BaseController < ApplicationController
+
+  before_action :admin_required!
+
+  private
+
+  def admin_required!
+    unless current_user.is_a?(Admin)
+      redirect_to root_path, alert: 'You are not authorized to view this page.'
+    end
+  end
+
+end
--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,4 +1,4 @@
-class Admin::TestsController < ApplicationController
+class Admin::TestsController < Admin::BaseController
 
   before_action :set_test, only: %i[show edit update destroy start]
 
