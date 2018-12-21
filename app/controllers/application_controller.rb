455279e88af5fa4f3939c3e7126620f99b1f2bc2
aritode <arian2k@gmail.com>
2018-12-21 16:45:01 +0300

    add Custom flash notice: (Hello, full_name)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,11 +16,12 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-      if resource.is_a?(Admin)
-        admin_tests_path
-      else
-        super
-      end
+    flash[:notice] = "Hello, #{current_user.full_name}!"
+    if resource.is_a?(Admin)
+      admin_tests_path
+    else
+      super
+    end
   end
 
 end
