c4a39d9f0337dafaf8abfde3c7e013ca727aeb3e
aritode <arian2k@gmail.com>
2018-12-21 16:11:51 +0300

    add redirect to /admin/tests to Admin after sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,12 @@ class ApplicationController < ActionController::Base
     current_user.type == 'Admin'
   end
 
+  def after_sign_in_path_for(resource)
+      if resource.is_a?(Admin)
+        admin_tests_path
+      else
+        super
+      end
+  end
+
 end
