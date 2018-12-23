21c9f0e1b26feb10bf1ba0ac514596e0bc122472
aritode <arian2k@gmail.com>
2018-12-23 21:28:31 +0300

    remove redundant #commented-code from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,10 @@ Rails.application.routes.draw do
   devise_for :users, path: :gurus, path_names: { sign_in: :login , sign_out: :logout }
 
   resources :tests, only: :index do
-    # resources :questions, shallow: true, except: :index do
-    #   resources :answers, shallow: true, except: :index
-    # end
-
     member do
       post :start
     end
   end
-
-  # get '/tests/:category/:title', to: 'tests#search', level: 2
 
   # GET /test_passages/101/result
   resources :test_passages, only: %i[show update] do
