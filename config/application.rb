12c826c97cf52574dac0507db919843769893cee
aritode <arian2k@gmail.com>
2018-12-25 01:24:18 +0300

    remove redundant code

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module TestGuru
     config.load_defaults 5.2
 
     config.time_zone = 'Moscow'
-    config.i18n.default_locale :ru
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
