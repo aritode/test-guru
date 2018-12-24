dc43399714aab3f7258f6402123f0a7a0f992455
aritode <arian2k@gmail.com>
2018-12-25 00:34:23 +0300

    add initial TestsMaler#completed_test localization (HTML/PlainText)

--- app/mailers/tests_mailer.rb
+++ app/mailers/tests_mailer.rb
@@ -4,7 +4,7 @@ class TestsMailer < ApplicationMailer
     @user = test_passage.user
     @test = test_passage.test
 
-    mail to: @user.email, subject: 'You just completed the TestGuru test!'
+    mail to: @user.email
   end
 
 end
