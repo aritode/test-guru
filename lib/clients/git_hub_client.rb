38f30b93b15c2fd19811fe56e5dc4f0bf6141f04
aritode <arian2k@gmail.com>
2019-03-28 13:47:52 +0300

    add #setup_http_client to GitHubClient (use Faraday gem)

--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -1,3 +1,15 @@
 class GitHubClient
 
+  ROOT_ENDPOINT = 'https://api.github.com'
+
+  def initialize
+    @http_client = setup_http_client
+  end
+
+  private
+
+  def setup_http_client
+    Faraday.new(url: ROOT_ENDPOINT)
+  end
+
 end
