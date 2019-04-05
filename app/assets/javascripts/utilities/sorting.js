0e30ce227f08ba07e5d8ce8281a897eb19487dba
aritode <arian2k@gmail.com>
2019-04-06 02:50:51 +0300

    add pointers and Asc/Desc sorting to sorting.js

--- app/assets/javascripts/utilities/sorting.js
+++ app/assets/javascripts/utilities/sorting.js
@@ -17,7 +17,15 @@ function sortRowsByTitle() {
         sortedRows.push(rows[i])
     }
 
-    sortedRows.sort(compareRows)
+    if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
+        sortedRows.sort(compareRowsAsc)
+        this.querySelector('.octicon-arrow-up').classList.remove('hide')
+        this.querySelector('.octicon-arrow-down').classList.add('hide')
+    } else {
+        sortedRows.sort(compareRowsDesc)
+        this.querySelector('.octicon-arrow-down').classList.remove('hide')
+        this.querySelector('.octicon-arrow-up').classList.add('hide')
+    }
 
     var sortedTable = document.createElement('table')
 
@@ -31,11 +39,20 @@ function sortRowsByTitle() {
     table.parentNode.replaceChild(sortedTable, table)
 }
 
-function compareRows(row1, row2) {
+function compareRowsAsc(row1, row2) {
     var testTitle1 = row1.querySelector('td').textContent
     var testTitle2 = row2.querySelector('td').textContent
 
     if (testTitle1 < testTitle2) { return -1 }
     if (testTitle1 > testTitle2) { return 1 }
+    return 0
+}
+
+function compareRowsDesc(row1, row2) {
+    var testTitle1 = row1.querySelector('td').textContent
+    var testTitle2 = row2.querySelector('td').textContent
+
+    if (testTitle1 > testTitle2) { return -1 }
+    if (testTitle1 < testTitle2) { return 1 }
     return 0
 }
