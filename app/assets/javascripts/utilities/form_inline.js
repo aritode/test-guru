ab60efcde9406e41a26df2356a7139df7673a313
aritode <arian2k@gmail.com>
2019-04-16 22:06:54 +0300

    refactor form_inline.js with jQuery

--- app/assets/javascripts/utilities/form_inline.js
+++ app/assets/javascripts/utilities/form_inline.js
@@ -1,11 +1,5 @@
 document.addEventListener('turbolinks:load', function () {
-    var controls = document.querySelectorAll('.form-inline-link')
-
-    if (controls.length) {
-        for (var i = 0; i < controls.length; i++) {
-            controls[i].addEventListener('click', formInlineLinkHandler)
-        }
-    }
+    $('.form-inline-link').on('click', formInlineLinkHandler)
 
     var errors = document.querySelector('.resource-errors')
 
@@ -24,16 +18,15 @@ function formInlineLinkHandler(event) {
 
 function formInlineHandler(testId) {
     var link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
-    var testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]')
-    var formInline = document.querySelector('.form-inline[data-test-id="' + testId + '"]')
+    var $testTitle = $('.test-title[data-test-id="' + testId + '"]')
+    var $formInline = $('.form-inline[data-test-id="' + testId + '"]')
 
-    if (formInline.classList.contains('hide')) {
-        testTitle.classList.add('hide')
-        formInline.classList.remove('hide')
+    $formInline.toggle()
+    $testTitle.toggle()
+
+    if ($formInline.is(':visible')) {
         link.textContent = 'Cancel'
     } else {
-        testTitle.classList.remove('hide')
-        formInline.classList.add('hide')
         link.textContent = 'Edit'
     }
 }
