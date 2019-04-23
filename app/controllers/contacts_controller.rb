c57ef37645fe042e3741cc08bcf6ef02619a7c52
aritode <arian2k@gmail.com>
2019-04-23 08:28:01 +0300

    add Contact Us - model, view and controller

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,23 @@
+class ContactsController < ApplicationController
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      redirect_to new_contact_path, notice: t('.success')
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :body)
+  end
+
+end
