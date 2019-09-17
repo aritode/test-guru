a5076e0f705ca7d90da3a5267538f6a41241c21e
aritode <arian2k@gmail.com>
2019-09-17 22:23:41 +0300

    Refactor BadgesHelper#badge_image

--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -1,9 +1,9 @@
 module BadgesHelper
 
-  DEFAULT_BADGE_IMAGE_URL = "badges/achievement_bronze.png"
+  DEFAULT_BADGE_IMAGE_URL = 'badges/achievement_bronze.png'
   BADGE_IMAGE_URLS = %w(badges/achievement_silver.png badges/achievement_gold.png)
 
-  def badge_image_helper(image_url: image_url, image_size: image_size)
+  def badge_image(image_url: image_url, image_size: image_size)
     badge_image_size = image_size == "small" ? "badge-image-small" : "badge-image"
     image_tag image_url, class: badge_image_size
   rescue Sprockets::Rails::Helper::AssetNotFound
